876ddd8c0c8be02f25c5f27baf81c05ac983da1d
Brian Romanchuk <brianr747@gmail.com>
2017-05-25 11:04:02 -0400

    Intro examples

---
+++ sfc_models/examples/scripts/intro_5_03_no_tax_SIM.py
@@ -0,0 +1,51 @@
+"""
+intro_5_03_no_tax_SIM.py
+
+Build a vesion of the SIM model (G&L Chapter 3) with no taxes,
+using sfc_model classes.
+
+(Supply siders run amok!)
+
+Copyright 2017 Brian Romanchuk
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+# This next line imports all of the objects used.
+# The syntax "from <module> import *" is frowned upon, but I want to reduce the number of lines of
+# code in these simpler examples.
+from sfc_models.objects import *
+from sfc_models.examples.Quick2DPlot import Quick2DPlot
+
+# The next line of code sets the name of the output files based on the code file's name.
+# This means that if you paste this code into a new file, get a new log name.
+register_standard_logs('output', __file__)
+# Create model, which holds all entities
+mod = Model()
+# Create first country - Canada. (This model only has one country.)
+can = Country(mod, 'CA', 'Canada')
+# Create sectors
+gov = ConsolidatedGovernment(can, 'GOV', 'Government')
+hh = Household(can, 'HH', 'Household')
+# A literally non-profit business sector
+bus = FixedMarginBusiness(can, 'BUS', 'Business Sector')
+labour = Market(can, 'LAB', 'Labour market')
+goods = Market(can, 'GOOD', 'Goods market')
+# Need to set the exogenous variable - Government demand for Goods ("G" in economist symbology)
+mod.AddExogenous('GOV', 'DEM_GOOD', '[20.,] * 105')
+# Build the model
+mod.main()
+CUT = 10
+k = mod.GetTimeSeries('k', cutoff=CUT)
+goods_produced = mod.GetTimeSeries('BUS__SUP_GOOD', cutoff=CUT)
+Quick2DPlot(k, goods_produced, 'Goods Produced (National Output)')
--- sfc_models/examples/scripts/intro_X_XX_externalsector.py
+++ sfc_models/examples/scripts/intro_X_XX_externalsector.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
 from sfc_models.objects import *
 from sfc_models.sector import Sector
+from sfc_models.examples.Quick2DPlot import Quick2DPlot
 
+register_standard_logs('output', __file__)
 mod = Model()
 ExternalSector(mod)
 ca = Country(mod, 'CA', currency='CAD')
@@ -34,6 +36,7 @@ hh_ca = Sector(ca, 'HH', has_F=True)
 hh_ca.AddVariable('GIFT', 'Sending money..', '5.')
 hh_us = Sector(us, 'HH', has_F=True)
 mod.RegisterCashFlow(hh_ca, hh_us, 'GIFT')
+mod.EquationSolver.TraceStep = 1
 mod.main()
 mod.TimeSeriesCutoff=1
 series_list = ('CA_HH__F', 'US_HH__F', 'EXT_FX__NET_CAD', 'EXT_FX__NET_USD')
@@ -45,6 +48,14 @@ series_list = ('CA_GOV__GOLDPURCHASES', 'US_GOV__GOLDPURCHASES', )
 print('Net balance fixed')
 for s in series_list:
     print(s, mod.GetTimeSeries(s)[1])
+
+# Get the data...
+source = mod.EquationSolver.TimeSeriesStepTrace
+x = source['iteration']
+y = source['CA_GOV__GOLDPURCHASES']
+stop = 50
+Quick2DPlot(x[0:stop], y[0:stop], 'Solution evolution for gold purchases',
+            filename='intro_goldstandard_iteration.png')
 
 
 
